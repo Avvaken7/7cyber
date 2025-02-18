ef7396455afaef1aa9dbccf2b3c8bcd04dd61a7b
Slava <avvaken7@gmail.com>
2025-02-18 22:59:22 +0200

    add categories section with swiper js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import './style.css'
+import './style.css';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+        modules: [Navigation],
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 6,
+        navigation: {
+                nextEl: '.custom-next',
+                prevEl: '.custom-prev',
+        },
+});
